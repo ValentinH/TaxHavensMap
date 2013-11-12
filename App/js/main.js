faee9c4bae148b7d2bcaa293e0c89b033efec475
Valentin Hervieu <enforce46@hotmail.fr>
2013-11-12 11:52:57 +0100

    Ajout des liens

--- App/js/main.js
+++ App/js/main.js
@@ -1,14 +1,15 @@
 $(function() {
-
- init();
-
-
+  init();
 });
 
 var data, values, files = ['./data/ftse100.json', './data/cac40.json'];
+var sourceCoords = [["51.51121","-0.11982"], ["48.8566","2.35097"]];
+var map, group, currentVal, min, max;
+var currentMap;
 function init()
 {
-  loadJson(0);
+  currentMap = 0;
+  loadJson();
 
   $("#right-panel").hover(function() {
     $( this ).stop().animate({
@@ -23,23 +24,26 @@ function init()
   });
 
   $("#map-chooser input[name='options']").change(function(){
-    var id = parseInt($(this).val());
-    loadJson(id);
+    currentMap = parseInt($(this).val());
+    loadJson();
   });
 }
 
-function loadJson(id)
+function loadJson()
 {
  $('#map').html("");
  $('#loader').show();
- $.getJSON(files[id], function(d){
+ $.getJSON(files[currentMap], function(d){
   data = d;
   index = 0;
-  values = [];
+  min = 100, max = 0;
   $("#right-panel>div").html("");
   for (var key in data) {
     $("#right-panel>div").append('<a href="#" id="val-'+key+'" onclick="showMap(\''+key+'\')" class="list-group-item">'+data[key].name+"</a>");
-    values = Array.prototype.concat.apply(values, jvm.values(data[key].values));
+    $.each( data[key].values, function(i, val){
+      if(val<min) min = parseInt(val);
+      if(val > max) max = parseInt(val);
+    });
   }
   showMap(0);
 });
@@ -47,41 +51,43 @@ function loadJson(id)
 
 function showMap(val)
 {
- $('#title').html(data[val].name);
+  currentVal = val;
+  $('#title').html(data[currentVal].name);
 
- $('a').removeClass("active");
- $('#val-'+val).addClass("active");
-
- $('#map').html("").vectorMap({
-  zoomMax: 5,
-  markers: data[val].coords,
-  series: {
-    markers: [
-    {
-      attribute: 'fill',
-      scale: ['#d3494e', '#d10b13'],
-      values: data[val].values,
-      min: jvm.min(values),
-      max: jvm.max(values)
+  $('a').removeClass("active");
+  $('#val-'+currentVal).addClass("active");
+  $("#map").html("");
+  map = new jvm.WorldMap({
+    container: $('#map'),
+    zoomMax: 5,
+    markers: data[currentVal].coords,
+    series: {
+      markers: [
+      {
+        attribute: 'fill',
+        scale: ['#d3494e', '#d10b13'],
+        values: data[currentVal].values,
+        min: min,
+        max: max
+      },
+      {
+        attribute: 'r',
+        scale: [5, 30],
+        values: data[currentVal].values,
+        min: min,
+        max: max
+      }]
     },
+    onMarkerLabelShow: function(event, label, index){
+      var nb = 0;
+      if(data[currentVal].values[index]) nb = data[currentVal].values[index];
+      label.html(
+        '<b>'+data[val].countries[index]+'</b><br/>'+
+        'Subsidiaries: <b>'+nb+'</b>'
+        );
+    },
+    onRegionLabelShow: function(event, label, code)
     {
-      attribute: 'r',
-      scale: [5, 30],
-      values: data[val].values,
-      min: jvm.min(values),
-      max: jvm.max(values)
-    }]
-  },
-  onMarkerLabelShow: function(event, label, index){
-    var nb = 0;
-    if(data[val].values[index]) nb = data[val].values[index];
-    label.html(
-      '<b>'+data[val].countries[index]+'</b><br/>'+
-      'Subsidiaries: <b>'+nb+'</b>'
-      );
-  },
-  onRegionLabelShow: function(event, label, code)
-  {
     //hide labels fro regions          
     label.hide();
     event.preventDefault();
@@ -92,39 +98,45 @@ function showMap(val)
   },
   onViewportChange : function(e, scale)
   {    
-    var s = Snap("#map svg");
-    //var g = s.selectAll("g")[1];
-    var mapObject = $('#map').vectorMap('get', 'mapObject');
-    var pt = mapObject.latLngToPoint("48.8566","2.35097");
-    if(group != null)
-     group.remove();
-
-    group = s.group();
-
-    circle = group.circle(pt.x,pt.y,10*scale);
-    circle.attr({
-        dataIndex:"5",
-        stroke: "#3F3",
-        strokeWidth: 3,
-        class: "jvectormap-marker jvectormap-element"
-    });
-    for(var i = 0; i < 100; i++)
-    {
-
-    var pt2 = mapObject.latLngToPoint("55.7558", "37.6176");
-    line = group.line(pt.x+i*10,pt.y+i*10,pt2.x+i*10,pt2.y+i*10);
-    line.attr({
-        strokeLinecap: "round",
-        stroke: "#F33",
-        strokeWidth: 10,
-        class: "line"
-    });
-  }
+    drawLinks(scale);
   }  
 });
 
- $('#loader').hide();
- $('.line').click(function(){alert("ok")});
+drawLinks(1);
+$('#loader').hide();
 }
 
-var group, line;
+function drawLinks(scale)
+{
+  if(map == null)
+    return;
+  var s = Snap("#map svg");
+  s = s.selectAll("g")[1];
+  if(group != null) group.remove();
+
+  group = s.group();
+
+  var source = sourceCoords[currentMap];
+  var pt = map.latLngToPoint(source[0],source[1]);
+  circle = group.circle(pt.x,pt.y,5*scale);
+  circle.attr({
+   fill : "#33F"
+  });
+
+  var coords = data[currentVal].coords;
+  $.each(coords, function(i, coord)
+  {
+    var pt2 = map.latLngToPoint(coord[0], coord[1]);
+    var value = data[currentVal].values[i];
+    var minStroke = 2, maxStroke = 30;
+    var strokeWidth = minStroke + (value - min) * (maxStroke-minStroke)/(max-min); 
+    console.log(min+" "+max);
+    line = group.line(pt.x,pt.y,pt2.x,pt2.y);
+    line.attr({
+      strokeLinecap: "round",
+      stroke: "#F33",
+      strokeWidth: strokeWidth,
+      class: "line"
+    });
+  }); 
+}
