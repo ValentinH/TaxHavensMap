3e6f407d65046fd75267578da9c425f6fb923049
Valentin Hervieu <enforce46@hotmail.fr>
2013-12-05 17:07:02 +0100

    Javascript code cleaning

--- App/js/main.js
+++ App/js/main.js
@@ -1,114 +1,108 @@
-$(function() {
-  init();
+$(function () {
+    init();
 });
 
 var cac40 = new MapData({
-  file : './data/cac40.json',
-  sourceCoords : {x: "48.8566", y: "2.35097"},
+    file: './data/cac40.json',
+    sourceCoords: {x: "48.8566", y: "2.35097"}
 });
 var ftse100 = new MapData({
-  file : './data/ftse100.json',
-  sourceCoords : {x: "51.51121", y: "-0.11982"},
+    file: './data/ftse100.json',
+    sourceCoords: {x: "51.51121", y: "-0.11982"}
 });
 var USA = new MapData({
-  file : './data/USA.json',
-  sourceCoords : {x: "40.714353", y: "-74.005973"},
+    file: './data/USA.json',
+    sourceCoords: {x: "40.714353", y: "-74.005973"}
 });
 
 var mapView = new MapView({
-  model : ftse100
+    model: ftse100
 });
 
-function init()
-{
-  loadJson();
-
-  //Set hover effect on side menu
-  $("#right-panel").hover(function() {
-    $( this ).stop().animate({
-      right: 0
-    }, 500);
-  },
-  function() {
-    $( this ).stop().animate({
-      right: -100
-    }, 500);
-  });
-
-  //set callback for the map switcher
-  $("#map-chooser input[name='options']").change(function(){
-      if(parseInt($(this).val()) == 0)
-        mapView.model = ftse100;
-      else if(parseInt($(this).val()) == 1)
-        mapView.model = cac40;
-      else if(parseInt($(this).val()) == 2)
-        mapView.model = USA;
+function init() {
     loadJson();
-  });
 
-  //set callback for the links switcher
-  $("#links-switcher input[name='options']").change(function(){
-      if($(this).val() == "true")
-        mapView.showLinks = true;
-      else        
-        mapView.showLinks = false;
-      mapView.drawLinks();
-      mapView.drawNodes();
-  });
-}
+    //Set hover effect on side menu
+    $("#right-panel").hover(function () {
+            $(this).stop().animate({
+                right: 0
+            }, 500);
+        },
+        function () {
+            $(this).stop().animate({
+                right: -100
+            }, 500);
+        });
 
-function loadJson()
-{
-  $('#map').html("");
-  $('#loader').show();
-  if(mapView.model.data == null)
-  {
-    $.getJSON(mapView.model.file, function(d){
-      mapView.model.data = d;
-      mapView.model.minValue = 100;
-      mapView.model.maxValue = 0;
-      for (var key in mapView.model.data) {
-        mapView.model.menuEl +='<a href="#" id="val-'+key+'" onclick="setCompany(\''+key+'\')" class="list-group-item">'+mapView.model.data[key].name+"</a>";
-          $.each( mapView.model.data[key].countries, function(i, country){
-            var val = country.value;
-            if(key != 0)
-            {
-              if(val < mapView.model.minValue) mapView.model.minValue = parseInt(val);
-              if(val > mapView.model.maxValue) mapView.model.maxValue = parseInt(val);
-            }
-            else
-            {
-              if(val < mapView.model.minTotalValue) mapView.model.minTotalValue = parseInt(val);
-              if(val > mapView.model.maxTotalValue) mapView.model.maxTotalValue = parseInt(val);
-            }
-          });
-      }
-      $("#right-panel>div").html(mapView.model.menuEl);
-      setCompany(0);
+    //set callback for the map switcher
+    $("#map-chooser").find("input[name='options']").change(function () {
+        if (parseInt($(this).val()) == 0)
+            mapView.model = ftse100;
+        else if (parseInt($(this).val()) == 1)
+            mapView.model = cac40;
+        else if (parseInt($(this).val()) == 2)
+            mapView.model = USA;
+        loadJson();
     });
-  }
-  else
-  {
-    $("#right-panel>div").html(mapView.model.menuEl);
-    setCompany(0);
-  }
+
+    //set callback for the links switcher
+    $("#links-switcher").find("input[name='options']").change(function () {
+        if ($(this).val() == "true")
+            mapView.showLinks = true;
+        else
+            mapView.showLinks = false;
+        mapView.drawLinks();
+        mapView.drawNodes();
+    });
 }
 
-function setCompany(val)
-{
-  $('#loader').show();
-  mapView.model.currentCompany = val;
-  mapView.drawMap();
+function loadJson() {
+    $('#map').html("");
+    $('#loader').show();
+    if (mapView.model.data == null) {
+        $.getJSON(mapView.model.file, function (d) {
+            mapView.model.data = d;
+            mapView.model.minValue = 100;
+            mapView.model.maxValue = 0;
+            for (var key in mapView.model.data) {
+                mapView.model.menuEl += '<a href="#" id="val-' + key + '" onclick="setCompany(\'' + key + '\')" class="list-group-item">' + mapView.model.data[key].name + "</a>";
+                $.each(mapView.model.data[key].countries, function (i, country) {
+                    var val = country.value;
+                    if (key != 0) {
+                        if (val < mapView.model.minValue) mapView.model.minValue = parseInt(val);
+                        if (val > mapView.model.maxValue) mapView.model.maxValue = parseInt(val);
+                    }
+                    else {
+                        if (val < mapView.model.minTotalValue) mapView.model.minTotalValue = parseInt(val);
+                        if (val > mapView.model.maxTotalValue) mapView.model.maxTotalValue = parseInt(val);
+                    }
+                });
+            }
+            $("#right-panel").find(">div").html(mapView.model.menuEl);
+            setCompany(0);
+        });
+    }
+    else {
+        $("#right-panel").find(">div").html(mapView.model.menuEl);
+        setCompany(0);
+    }
+}
 
-  //update the title
-  $('#title').html(mapView.model.data[mapView.model.currentCompany].name);
+function setCompany(val) {
+    var loaderSelector = $('#loader');
+    loaderSelector.show();
+    mapView.model.currentCompany = val;
+    mapView.drawMap();
 
-  //update the menu view
-  $('a').removeClass("active");
-  $('#val-'+mapView.model.currentCompany).addClass("active");
+    //update the title
+    $('#title').html(mapView.model.data[mapView.model.currentCompany].name);
 
-  //scale 1
-  mapView.drawLinks();
-  mapView.drawNodes();
-  $('#loader').hide();
+    //update the menu view
+    $('a').removeClass("active");
+    $('#val-' + mapView.model.currentCompany).addClass("active");
+
+    //scale 1
+    mapView.drawLinks();
+    mapView.drawNodes();
+    loaderSelector.hide();
 }
--- App/js/model.js
+++ App/js/model.js
@@ -12,7 +12,7 @@ function MapData(args) {
 }
 
 function MapView(args) {
-    args = args || {};	
+    args = args || {};
     this.model = args.model || null;
     this.map = null;
     this.minNodeSize = args.minNodeSize || 5;
@@ -25,211 +25,196 @@ function MapView(args) {
     this.nodesLayer = null;
     this.showLinks = false;
     this.currentScale = 1;
+    this.labelSelector = null;
 }
 
-MapView.prototype.drawMap = function(){		
-    var min = this.model.minValue;
-    var max = this.model.maxValue;
-    if(this.model.currentCompany == 0)
-    {
-        min = this.model.minTotalValue;
-        max = this.model.maxTotalValue;
-    }
-
+MapView.prototype.drawMap = function () {
+    this.labelSelector = $(".my-label");
     //reset the map
     $("#map").html("");
     $(".jvectormap-label").remove();
-    self = this;
+    var self = this;
     this.map = new jvm.WorldMap({
         container: $('#map'),
         zoomMax: 5,
-    onRegionLabelShow: function(event, label, code) {
-        //hide labels from regions          
-        label.hide();
-        event.preventDefault();
-    },
-    onRegionOver: function(event, code) {
-        event.preventDefault();
-    },
-    onViewportChange : function(e, scale) { 
-        self.currentScale = scale;
-        self.drawLinks();   
-        self.drawNodes();
-        self.out();
-    }  
+        onRegionLabelShow: function (event, label, code) {
+            //hide labels from regions
+            label.hide();
+            event.preventDefault();
+        },
+        onRegionOver: function (event, code) {
+            event.preventDefault();
+        },
+        onViewportChange: function (e, scale) {
+            self.currentScale = scale;
+            self.drawLinks();
+            self.drawNodes();
+            self.out(e);
+        }
     });
 };
 
-MapView.prototype.drawNodes = function()
-{
-    if(this.map == null)
+MapView.prototype.drawNodes = function () {
+    if (this.map == null)
         return;
     var self = this;
     var min = this.model.minValue;
     var max = this.model.maxValue;
     var scale = self.currentScale;
-    if(this.model.currentCompany == 0)
-    {
+    if (this.model.currentCompany == 0) {
         min = this.model.minTotalValue;
         max = this.model.maxTotalValue;
     }
 
     var s = Snap("#map svg");
-    if(this.nodesLayer != null) this.nodesLayer.remove();
+    if (this.nodesLayer != null) this.nodesLayer.remove();
     this.nodesLayer = s.group();
 
-    $.each(this.model.data[this.model.currentCompany].countries, function(i, country)
-    {
+    $.each(this.model.data[this.model.currentCompany].countries, function (i, country) {
         var value = country.value;
         var name = country.name;
         var coord = country.coords;
 
         var cScale = new jvm.ColorScale(self.colorScale, "polynomial", min, max);
-        var radius = self.minNodeSize + (value - min) * (self.maxNodeSize-self.minNodeSize)/(max-min); 
-           
+        var radius = self.minNodeSize + (value - min) * (self.maxNodeSize - self.minNodeSize) / (max - min);
+
         //draw the destination point
         var pt = self.map.latLngToPoint(coord[0], coord[1]);
-        var circle = self.nodesLayer.circle(pt.x, pt.y, radius*Math.sqrt(scale));
+        var circle = self.nodesLayer.circle(pt.x, pt.y, radius * Math.sqrt(scale));
         circle.attr({
-            fill : cScale.getValue(value),            
+            fill: cScale.getValue(value),
             stroke: '#505050',
-            "stroke-width": 1,
+            "stroke-width": 1
         });
         circle.hover(
-        function(){
-            this.attr({
-                stroke: 'black',
-                "stroke-width": 2
-            });
-        },        
-        function(){
-            this.attr({
-                fill : cScale.getValue(value),            
-                stroke: '#505050',
-                "stroke-width": 1
-            });
-        }
+            function () {
+                this.attr({
+                    stroke: 'black',
+                    "stroke-width": 2
+                });
+            },
+            function () {
+                this.attr({
+                    fill: cScale.getValue(value),
+                    stroke: '#505050',
+                    "stroke-width": 1
+                });
+            }
         );
         circle.data("index", i);
-        circle.mousemove( function(e){
-            $(".my-label").show();      
-            self.displayLabel($(".my-label"), this.data("index"));  
+        circle.mousemove(function (e) {
+            self.labelSelector.show();
+            self.displayLabel(self.labelSelector, this.data("index"));
             self.positionLabel(e);
         });
-        circle.touchstart( function(e){           
-            $(".my-label").show();      
-            self.displayLabel($(".my-label"), this.data("index"));  
-            self.positionLabel({x:e.touches[0].clientX, y : e.touches[0].clientY});
+        circle.touchstart(function (e) {
+            self.labelSelector.show();
+            self.displayLabel(self.labelSelector, this.data("index"));
+            self.positionLabel({x: e.touches[0].clientX, y: e.touches[0].clientY});
         });
         circle.mouseout(self.out);
     });
     //draw the source point
     var source = this.model.sourceCoords;
-    var sourcePt = this.map.latLngToPoint(source.x,source.y);
-    var circle = this.nodesLayer.circle(sourcePt.x,sourcePt.y,10*scale);
+    var sourcePt = this.map.latLngToPoint(source.x, source.y);
+    var circle = this.nodesLayer.circle(sourcePt.x, sourcePt.y, 10 * scale);
     circle.attr({
-        fill : this.sourceColor
+        fill: this.sourceColor
     });
-    circle.mousemove(function(e){
-        $(".my-label").show();      
-        $(".my-label").html('<b>Source</b>');
+    circle.mousemove(function (e) {
+        self.labelSelector.show();
+        self.labelSelector.html('<b>Source</b>');
         self.positionLabel(e);
-    });   
-    circle.touchstart(function(e){
-        $(".my-label").show();      
-        $(".my-label").html('<b>Source</b>');
-        self.positionLabel({x:e.touches[0].clientX, y : e.touches[0].clientY});
-    });            
+    });
+    circle.touchstart(function (e) {
+        self.labelSelector.show();
+        self.labelSelector.html('<b>Source</b>');
+        self.positionLabel({x: e.touches[0].clientX, y: e.touches[0].clientY});
+    });
     circle.mouseout(self.out);
 };
-MapView.prototype.out = function(e) {       
-        $(".my-label").hide();
+MapView.prototype.out = function (e) {
+    $(".my-label").hide();
 };
 
 
-MapView.prototype.drawLinks = function() {
-    if(this.map == null)
+MapView.prototype.drawLinks = function () {
+    if (this.map == null)
         return;
     var self = this;
-
     var scale = self.currentScale;
     var s = Snap("#map svg");
-    if(this.linksLayer != null) this.linksLayer.remove();
+    if (this.linksLayer != null) this.linksLayer.remove();
     this.linksLayer = s.group();
 
     var source = this.model.sourceCoords;
-    var pt = this.map.latLngToPoint(source.x,source.y);
+    var pt = this.map.latLngToPoint(source.x, source.y);
 
-    if(this.showLinks)
-    {
+    if (this.showLinks) {
         var min = this.model.minValue;
         var max = this.model.maxValue;
-        if(this.model.currentCompany == 0)
-        {
+        if (this.model.currentCompany == 0) {
             min = this.model.minTotalValue;
             max = this.model.maxTotalValue;
         }
 
         //draw all the links
         var cScale = new jvm.ColorScale(this.colorScale, "polynomial", min, max);
-        $.each(this.model.data[this.model.currentCompany].countries, function(i, country)
-        {            
+        $.each(this.model.data[this.model.currentCompany].countries, function (i, country) {
             var value = country.value;
-            var coord = country.coords;
+            var coords = country.coords;
 
-            var pt2 = self.map.latLngToPoint(coord[0], coord[1]);
+            var pt2 = self.map.latLngToPoint(coords[0], coords[1]);
             var controlPointX = (pt.x + pt2.x) / 2 + (pt2.y - pt.y) / 4,
-            controlPointY = (pt.y + pt2.y) / 2 + (pt.x - pt2.x) / 4;
+                controlPointY = (pt.y + pt2.y) / 2 + (pt.x - pt2.x) / 4;
 
-            var strokeWidth = self.minLinkSize + (value - min) * (self.maxLinkSize-self.minLinkSize)/(max-min); 
+            var strokeWidth = self.minLinkSize + (value - min) * (self.maxLinkSize - self.minLinkSize) / (max - min);
             strokeWidth *= Math.sqrt(scale);
-            line = self.linksLayer.path("M"+pt.x+","+pt.y+" Q"+controlPointX+","+controlPointY+" "+
-                pt2.x+","+pt2.y);
+            var line = self.linksLayer.path("M" + pt.x + "," + pt.y + " Q" + controlPointX + "," + controlPointY + " " +
+                pt2.x + "," + pt2.y);
             line.attr({
                 strokeLinecap: "round",
                 stroke: cScale.getValue(value),
                 strokeWidth: strokeWidth,
-                fill : 'none',
+                fill: 'none',
                 class: "line",
-                index : i
+                index: i
             });
-            line.data("index", i);            
-            line.mousemove( function(e){               
-                $(".my-label").show();      
-                self.displayLabel($(".my-label"), this.data("index"));  
+            line.data("index", i);
+            line.mousemove(function (e) {
+                self.labelSelector.show();
+                self.displayLabel(self.labelSelector, this.data("index"));
                 self.positionLabel(e);
             });
-            line.touchstart( function(e){
-                $(".my-label").show();      
-                self.displayLabel($(".my-label"), this.data("index"));
-                self.positionLabel({x:e.touches[0].clientX, y : e.touches[0].clientY});
+            line.touchstart(function (e) {
+                self.labelSelector.show();
+                self.displayLabel(self.labelSelector, this.data("index"));
+                self.positionLabel({x: e.touches[0].clientX, y: e.touches[0].clientY});
             });
             line.mouseout(self.out);
-            });
+        });
     }
 };
 
-MapView.prototype.displayLabel = function(label, index)
-{
+MapView.prototype.displayLabel = function (label, index) {
     var nb = 0;
-    if(self.model.data[self.model.currentCompany].countries[index].value) nb = self.model.data[self.model.currentCompany].countries[index].value;
+    if (this.model.data[this.model.currentCompany].countries[index].value) nb = this.model.data[this.model.currentCompany].countries[index].value;
     label.html(
-        '<b>'+self.model.data[self.model.currentCompany].countries[index].name+'</b><br/>'+
-        'Subsidiaries: <b>'+nb+'</b>'
-        );
-},
+        '<b>' + this.model.data[this.model.currentCompany].countries[index].name + '</b><br/>' +
+            'Subsidiaries: <b>' + nb + '</b>'
+    );
+};
 
-MapView.prototype.positionLabel = function(pos)
-{
-    var left = pos.x-15-parseInt($(".my-label").width()),
-    top = pos.y-15-parseInt($(".my-label").height());
+MapView.prototype.positionLabel = function (pos) {
+    var left = pos.x - 15 - parseInt(this.labelSelector.width()),
+        top = pos.y - 15 - parseInt(this.labelSelector.height());
     if (left < 5) {
-        left = pos.x+15;
+        left = pos.x + 15;
     }
     if (top < 5) {
         top = pos.y + 15;
     }
-    $(".my-label").css({
+    this.labelSelector.css({
         left: left,
         top: top
     });
